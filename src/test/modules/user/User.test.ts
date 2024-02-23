39470d289d33266317399bd5d02e9c915242a3be
Paulo Wellington <paulo@yaayoo.com.br>
2024-02-23 17:15:40 -0300

    feat: Implementing Unit Test

---
+++ src/test/modules/user/User.test.ts
@@ -0,0 +1,21 @@
+describe("User Module", () => {
+
+    it("is just a test", () => {
+        const awaited = 5;
+        const response = (2 + 3);
+
+        expect(response).toBe(awaited);
+    });
+
+});
+
+describe("Accomodation Module", () => {
+
+    it("must return an accomodation", () => {
+        const awaited = "accomodation";
+        const response = "accomodation";
+
+        expect(response).toBe(awaited);
+    })
+
+})
